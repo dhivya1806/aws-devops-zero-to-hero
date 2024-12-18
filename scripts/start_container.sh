4c5a0f1872169f4e43947f29852be0e052e083b5
dhivya1806 <92577481+dhivya1806@users.noreply.github.com>
2024-12-18 12:39:27 +0530

    removed sudo command

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull dhivya1806/codecommit-demo:latest
+docker pull dhivya1806/code-build-pythondemo:latest
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 dhivya1806/codecommit-demo
+docker run -d -p 5000:5000 dhivya1806/code-build-pythondemo
