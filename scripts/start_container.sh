5f9d0d16ee85a11f0b9c871fb9c40f950dd3241e
dhivya <dhivyams97@gmail.com>
2025-02-21 07:56:50 +0000

    updated containerid

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-containerID=`sudo docker ps | awk -F " " '{print $1}'`
-docker rm -f $containerID
 
 # Pull the Docker image from Docker Hub
 sudo docker pull dhivya1806/code-build-pythondemo:latest
