260f9dd93f8a9742caad199bb4938e711a71d44e
dhivya1806 <92577481+dhivya1806@users.noreply.github.com>
2025-02-22 10:48:36 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 
 # Pull the Docker image from Docker Hub
-sudo docker pull dhivya1806/code-build-pythondemo:latest
+sudo docker pull dhivya1806/simple-python-flask-app-demo:latest
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 dhivya1806/code-build-pythondemo
+sudo docker run -d -p 5000:5000 dhivya1806/simple-python-flask-app-demo
