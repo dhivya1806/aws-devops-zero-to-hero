f8ed558ef304ad589c5f81466b947752eba9e886
dhivya1806 <92577481+dhivya1806@users.noreply.github.com>
2025-02-22 12:36:59 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Hi"
+CONTAINER_ID=$(docker ps -q | head -n 1)
+
+if [ -n "$CONTAINER_ID" ]; then
+    echo "Stopping container: $CONTAINER_ID"
+    docker stop "$CONTAINER_ID"
+else
+    echo "No running containers found."
+fi
 
 
 
